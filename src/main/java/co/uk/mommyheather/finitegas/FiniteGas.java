70ee3041a52fe1481dd6f1dcc29b9e0f4ab800be
Heather White <66441550+MommyHeather@users.noreply.github.com>
2023-11-07 22:46:32 +0000

    Empty mod container - exists so it shows in Forge's modlist

---
+++ src/main/java/co/uk/mommyheather/finitegas/FiniteGas.java
@@ -0,0 +1,19 @@
+package co.uk.mommyheather.finitegas;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+
+
+@Mod(modid = FiniteGas.MODID, name = FiniteGas.NAME, version = FiniteGas.VERSION)
+@EventBusSubscriber
+public class FiniteGas
+{
+
+    
+    public static final String MODID = "finitegas";
+    public static final String NAME = "Finite Gas";
+    public static final String VERSION = "1.0";
+
+
+}
